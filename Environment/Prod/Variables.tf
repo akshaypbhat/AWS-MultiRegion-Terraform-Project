ebb22fd938ee647726af9933e6701f2915f536ad
Akshay B P <139035375+akshaypbhat@users.noreply.github.com>
2026-04-15 21:09:33 +0530

    Create Variables.tf

---
+++ Environment/Prod/Variables.tf
@@ -0,0 +1,16 @@
+variable "region_1_name" {
+  type = string
+}
+
+variable "region_2_name" {
+  type = string
+}
+
+variable "vpc_cidr_1" {
+  type = string
+}
+
+variable "vpc_cidr_2" {
+  type = string
+}
+
