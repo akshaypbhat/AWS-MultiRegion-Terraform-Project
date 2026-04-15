b0b029898d0f657f640c6e48ebb3813dd3c28050
Akshay B P <139035375+akshaypbhat@users.noreply.github.com>
2026-04-15 22:10:23 +0530

    Create Output.tf

---
+++ Modules/VPC/Output.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "The ID of the VPC"
+}
+
+output "public_subnet_id" {
+  value       = aws_subnet.public.id
+  description = "The ID of the public subnet"
+}
+
+output "private_subnet_id" {
+  value       = aws_subnet.private.id
+  description = "The ID of the private subnet"
+}
+
+output "private_route_table_id" {
+  value       = aws_route_table.private.id
+  description = "The ID of the private route table (needed for TGW routes)"
+}
+
