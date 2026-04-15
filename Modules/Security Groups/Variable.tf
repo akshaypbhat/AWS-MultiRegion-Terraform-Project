56265c8f4befc8a839f0d0007adceaaccc35ca4b
Akshay B P <139035375+akshaypbhat@users.noreply.github.com>
2026-04-15 21:36:13 +0530

    Create Variable.tf

---
+++ Modules/Security Groups/Variable.tf
@@ -0,0 +1,10 @@
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where the security group will be created"
+}
+
+variable "region_name" {
+  type        = string
+  description = "The name of the region for tagging"
+}
+
