d3a253b0c5502e3abdec7dce292ca970bca62839
Akshay B P <139035375+akshaypbhat@users.noreply.github.com>
2026-04-15 22:10:56 +0530

    Create Variable.tf

---
+++ Modules/VPC/Variable.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "region_name" {
+  type        = string
+  description = "The name of the AWS region"
+}
+
